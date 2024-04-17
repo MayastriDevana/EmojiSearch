cfd67ed31951a3c609acb6da55963467c2aa9884
Maya <Mayastridevana0205@gmail.com>
2024-04-17 08:54:19 +0700

    Initial Commit

---
+++ EmojiSearch/EmojiSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSearchApp.swift
+//  EmojiSearch
+//
+//  Created by User on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
