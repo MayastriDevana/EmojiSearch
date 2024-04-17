e1bc656f4ac057e139e3f79fadbde10c360be076
Maya <Mayastridevana0205@gmail.com>
2024-04-17 09:28:39 +0700

    Version Update

--- EmojiSearch/ContentView.swift
+++ EmojiSearch/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "person")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+                .foregroundStyle(.purple)
+            Text("v1.0.0")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
