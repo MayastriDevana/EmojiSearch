a07c30f8c6b5107a4dea1b81f77f4594825b1ace
Maya <Mayastridevana0205@gmail.com>
2024-04-17 10:21:50 +0700

    EmojiRow Component

---
+++ EmojiSearch/Utility/RandomColor.swift
@@ -0,0 +1,15 @@
+//
+//  RandomColor.swift
+//  EmojiSearch
+//
+//  Created by User on 17/04/24.
+//
+import SwiftUI
+
+struct RandomColor{
+    static func bgView() -> Color {
+        return Color(red: Double.random(in: 0...1),
+                     green: Double.random(in: 0...1),
+                     blue: Double.random(in: 0...1))
+    }
+}
