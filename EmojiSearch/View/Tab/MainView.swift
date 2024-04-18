b5c9d9ccd8247e5a445ab2b0bffafd73c8000f8b
Maya <Mayastridevana0205@gmail.com>
2024-04-18 09:25:46 +0700

    TabView

---
+++ EmojiSearch/View/Tab/MainView.swift
@@ -0,0 +1,33 @@
+//
+//  MainView.swift
+//  EmojiSearch
+//
+//  Created by User on 18/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            ContentView()
+                .tabItem {
+                    Label("Emoji", systemImage: "ev.plug.dc.nacs")
+                }
+            Text("Map View")
+                .tabItem {
+                    Label("Location", systemImage: "binoculars")
+                }
+                .badge(4)
+            Text("Setting Page")
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
+        .tint(.primary)
+    }
+}
+
+#Preview {
+    MainView()
+}
